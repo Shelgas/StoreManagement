de69bb164474aeda5d7537b6a77cb7870f5642f8
Shelgas <akozlov1997@gmail.com>
2023-06-28 22:20:27 +0300

    Changed the way the category entity is configured

--- src/SM.Infrastructure/EntityTypeConfigurations/CategoryConfiguration.cs
+++ src/SM.Infrastructure/EntityTypeConfigurations/CategoryConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SM.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace SM.Infrastructure.EntityTypeConfigurations
 {
-    internal class CategoryConfiguration
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(category => category.Id);
+            builder.HasIndex(category => category.Id);
+            builder.Property(category => category.Name).HasMaxLength(255);
+        }
     }
 }
